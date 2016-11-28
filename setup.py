823133cc0e9f02a9c2b3f6aebd30e35c7dc767fe
Tanner Grehawick <tgrehawi@gmail.com>
2016-11-28 18:49:42 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+setup(
+	name="markdowner",
+	version="0.dev0",
+	description="yet another markdown renderer",
+	url="https://github.com/tgrehawi/markdowner",
+	author="tgrehawi",
+	author_email="tgrehawi@gmail.com",
+	packages=find_packages(),
+	license="GPL",
+)
